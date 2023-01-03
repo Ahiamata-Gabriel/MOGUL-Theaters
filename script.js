702789400cec46f011f4f4c12e65bb494cecd2b1
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2023-01-03 00:17:09 +0000

    Movie app ui

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
-const imgs = document.getElementById('imgs');
-const leftBtn = document.getElementById('left');
-const rightBtn = document.getElementById('right');
-const img = document.querySelectorAll('#imgs img');
+// const imgs = document.getElementById('imgs');
+// const leftBtn = document.getElementById('left');
+// const rightBtn = document.getElementById('right');
+// const img = document.querySelectorAll('#imgs img');
 
-let idx = 0;
+// let idx = 0;
 
-const changeImage = () => {
-  if (idx > img.length - 1) {
-    idx = 0;
-  } else if (idx < 0) {
-    idx = img.length - 1;
-  }
+// const changeImage = () => {
+//   if (idx > img.length - 1) {
+//     idx = 0;
+//   } else if (idx < 0) {
+//     idx = img.length - 1;
+//   }
 
-  imgs.style.transform = `translateX(${-idx * 100}%)`;
-};
+//   imgs.style.transform = `translateX(${-idx * 100}%)`;
+// };
 
-const run = () => {
-  idx++;
-  changeImage();
-};
+// const run = () => {
+//   idx++;
+//   changeImage();
+// };
 
-let interval = setInterval(run, 5000);
+// let interval = setInterval(run, 5000);
 
-function resetInterval() {
-  clearInterval(interval);
-  interval = setInterval(run, 5000);
-}
+// function resetInterval() {
+//   clearInterval(interval);
+//   interval = setInterval(run, 5000);
+// }
 
-rightBtn.addEventListener('click', () => {
-  idx++;
-  changeImage();
-  resetInterval();
-});
+// rightBtn.addEventListener('click', () => {
+//   idx++;
+//   changeImage();
+//   resetInterval();
+// });
 
-leftBtn.addEventListener('click', () => {
-  idx--;
-  changeImage();
-  resetInterval();
-});
+// leftBtn.addEventListener('click', () => {
+//   idx--;
+//   changeImage();
+//   resetInterval();
+// });
+
+// dots.addEventListener('click', transform());
